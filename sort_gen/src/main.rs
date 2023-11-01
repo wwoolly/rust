002f74fb9b4c691d43483c871e87b7893ed4534d
Nikita Kokunin <larchflap@gmail.com>
2023-11-02 03:54:54 +0700

    sort_gen added one impl of struct Element and test

--- sort_gen/src/main.rs
+++ sort_gen/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::fmt;
 
 fn main() {
     println!("Hello, world!");
@@ -23,6 +24,7 @@ fn sort<T>(arr : &mut [T], cmp: fn(&T, &T) -> bool) {
     }
 }
 
+// #[derive(Clone, Copy)]e
 struct Element<T, U> {
     re : T,
     im : U
@@ -36,14 +38,18 @@ impl <T : PartialEq, U : PartialEq> PartialEq for Element<T, U> {
 
 impl <T : PartialEq, U : PartialEq> Eq for Element<T, U> {}
 
+impl <T : fmt::Display, U : fmt::Display> fmt::Display for Element<T, U> {
+    fn fmt(&self, f : &mut fmt::Formatter<'_>) -> fmt::Result { 
+        write!(f, "re = {}; im = {};", self.re, self.im)
+    }
+}
+
 impl <T, U> Element<T, U> {
     fn new(re: T, im: U) -> Self {
         Self { re, im }
     }
 }
 
-
-#[derive(Copy)]
 impl Element<i32, i32> {
     fn default() -> Self {
         let re = rand::thread_rng().gen_range(0..100);
@@ -55,51 +61,50 @@ impl Element<i32, i32> {
 
 #[test]
 fn test_sorting() {
-    let mut arr1 : [Element<i32, f32>; 3] = [
-        Element { re: (1i32), im: (2f32) },
-        Element { re: (2i32), im: (3f32) },
-        Element { re: (3i32), im: (4f32) }
-    ];
-
+    let mut arr1: [Element<i32, f32>; 3] = test_array_1_1();
+    
     sort(&mut arr1, |a : &Element<i32, f32>, b : &Element<i32, f32>| a.im > b.im);
-    assert_eq!([
-        Element { re: (1i32), im: (2f32) },
-        Element { re: (2i32), im: (3f32) },
-        Element { re: (3i32), im: (4f32) }
-    ], arr1);
+    assert!(compare_arrays(&sorted_array_1(), &arr1));
 
-    sort(&mut arr1, |a, b| a < b);
-    assert_eq!([5, 4, 3, 2, 1], arr1);
-
-
-    let mut arr2 = [1, 2, 3, 3, 5, 6];
-    sort(&mut arr2, |a, b| a > b);
-    assert_eq!([1, 2, 3, 3, 5, 6], arr2);
-
-    sort(&mut arr2, |a, b| a < b);
-    assert_eq!([6, 5, 3, 3, 2, 1], arr2);
-
-    sort(&mut arr2, |a, b| a > b);
-    assert_ne!([6, 5, 3, 3, 2, 1], arr2);
+    arr1 = test_array_1_2();
+    sort(&mut arr1, |a : &Element<i32, f32>, b : &Element<i32, f32>| a.im > b.im);
+    assert!(compare_arrays(&sorted_array_1(), &arr1));
 }
 
 // это не идеальная версия 
-fn compare_arrays<T, U>(arr1 : &mut [Element<T, U>], arr2 : &mut [Element<T, U>]) -> bool {
-    assert_eq!(arr1.len(), arr2.len());
+fn compare_arrays<T : PartialEq, U : PartialEq>(arr1 : &[Element<T, U>], arr2 : &[Element<T, U>]) -> bool
+    where T: PartialEq, 
+    T : fmt::Display,
+    U : PartialEq,
+    U : fmt::Display,
+    {
+    if arr1.len() != arr2.len() {
+        println!("Arrays length is not equal ({} != {})", arr1.len(), arr2.len());
+        return false;
+    }
 
     for i in 0..arr1.len() {
         if arr1[i] != arr2[i] {
-            panic!("Elements with index '{}' is not equal!", i);
+            println!("Elements with index '{}' is not equal! -- {} != {}", i, arr1[i], arr2[i]);
+            return false;
         }
     }
     return true;
 }
 
-fn test_array_1() -> [Element<i32, f32>; 3] {
+fn test_array_1_1() -> [Element<i32, f32>; 3] {
     return [
         Element { re: (3i32), im: (4f32) },
         Element { re: (1i32), im: (2f32) },
         Element { re: (2i32), im: (3f32) }
+    ];
+}
+
+fn test_array_1_2() -> [Element<i32, f32>; 3] {
+    return [
+        Element { re: (2i32), im: (3f32) },
+        Element { re: (3i32), im: (4f32) },
+        Element { re: (1i32), im: (2f32) }
     ];
 }
 
